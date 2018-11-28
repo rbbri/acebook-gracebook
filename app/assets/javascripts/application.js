e7fb7d527cbca0f981c9ed2ed4ce3f239fb64e9f
Irina Baldwin <2007inb@gmail.com>
2018-11-28 11:06:41 +0000

    Last save before branch from master

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,8 +9,3 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//= require jquery3
-//= require popper
-//= require bootstrap
-//= require rails-ujs
-//= require_tree .
