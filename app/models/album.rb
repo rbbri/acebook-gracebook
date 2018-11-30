af609a6323a2e40dab45771d4dd2a29fed11dcb7
Caitlin Cooling <ccooling97@gmail.com>
2018-11-30 13:16:32 +0000

    [WIP] Showing the images that you have added to an album

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,3 +1,3 @@
 class Album < ApplicationRecord
-  has_many_attached :images
+  has_one_attached :images
 end
--- spec/features/albums/user_can_upload_photos_to_album_spec.rb
+++ spec/features/albums/user_can_upload_photos_to_album_spec.rb
@@ -6,14 +6,14 @@ RSpec.feature 'Album', type: :feature do
 
   before do
     user = create(:user)
-  end
-
-  scenario "user can upload a photo" do
     visit('/')
     click_link 'Log in'
     fill_in 'Email', with: 'John@example.com'
     fill_in 'Password', with: 'sosecure'
     click_button 'Log in'
+  end
+
+  scenario "user can upload a photo" do
     visit '/albums'
     click_link 'New Album'
     fill_in 'album_title', with: 'New album'
@@ -23,4 +23,14 @@ RSpec.feature 'Album', type: :feature do
     # expect(page.find('.album')['src']).to have_content 'image.png'
   end
 
+  scenario "user can see album pictures" do
+    click_link 'Albums'
+    click_link 'New Album'
+    fill_in 'album_title', with: 'My first album'
+    attach_file("album[images][]", Rails.root + "spec/features/albums/image.png")
+    click_button 'Create Album'
+    click_link 'Back'
+    click_link 'Show'
+    expect(page.find('.album_images')['src']).to have_content 'image.png'
+  end
 end
